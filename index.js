740df14f8f6207a22b5f9ef632f6c4f9b36c120f
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-01-24 10:38:44 +0200

    3.2

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const app = express()
 
 app.use(express.json())
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
-
 let persons = [
     {
         id: 1,
@@ -32,10 +27,20 @@ let persons = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1>')
+    response.send(`<h1>Hello World</h1>`)
   })
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
-
 })
+
+app.get('/api/info', (request, response) => {
+    const datetime = new Date()
+    const ppl = persons.filter(x => x.id).length
+    return response.send(`<p> Phonebook has info for ${ppl} people</p><p>${datetime}</p>`)
+  })
+
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
