d4bf88eb101e65e038ddbed2c7bd784bd9f141ff
riikkayoki <74421508+riikkayoki@users.noreply.github.com>
2023-02-03 10:36:53 +0200

    3.12

---
+++ mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://riikkayoki:${password}@cluster0.7mjb6fm.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    name: name,
+    number: number,
+  })
+
+if (name && number) {
+    person.save().then(result => {
+        console.log(`added ${name} ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else {
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+        console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
